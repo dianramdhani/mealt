0a0f3895b99305c70422bddd7db34819fa1a1dde
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2019-09-23 19:27:36 +0700

    update config: move ep to meal

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 
     window.app
         .constant('CONFIG', {
-            API: 'http://ep.tritronik.com:8443',
+            API: 'http://meal.tritronik.com:8443',
             IMAGEURL: 'http://meal.tritronik.com/assets/img/meals/'
         });
 })();
