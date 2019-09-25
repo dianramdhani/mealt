7048b840772e72edb04772fe175bdd66ab3fce4e
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2019-09-25 18:44:15 +0700

    update meal: add reaction

--- views/meal/meal.js
+++ views/meal/meal.js
@@ -13,17 +13,19 @@ const feather = require('feather-icons');
             controller: _
         });
 
-    _.$inject = ['$scope', '$timeout', '$element', '$compile', 'MealRestService'];
-    function _($scope, $timeout, $element, $compile, MealRestService) {
-        let $ctrl = this;
-        $ctrl.$onInit = async () => {
+    _.$inject = ['$scope', '$timeout', '$element', '$compile', 'MealRestService', 'UserReactionRestService'];
+    function _($scope, $timeout, $element, $compile, MealRestService, UserReactionRestService) {
+        const reloadData = async () => {
             $scope.meals = false;
             $scope.meals = await MealRestService.getAllMeal().then(_ => _.data);
             $scope.$apply();
             $timeout(() => {
                 feather.replace();
             });
-
+        };
+        let $ctrl = this;
+        $ctrl.$onInit = () => {
+            reloadData();
             $scope.$watch('search', () => {
                 $timeout(() => {
                     feather.replace();
@@ -34,6 +36,21 @@ const feather = require('feather-icons');
         $scope.comment = (meal) => {
             $scope.meal = meal;
             $element.prepend($compile(`<comment-absolute meal="meal"></comment-absolute>`)($scope));
+        };
+
+        $scope.like = async (mealId) => {
+            await UserReactionRestService.like(mealId);
+            reloadData();
+        };
+
+        $scope.dislike = async (mealId) => {
+            await UserReactionRestService.dislike(mealId);
+            reloadData();
+        };
+
+        $scope.bored = async (mealId) => {
+            await UserReactionRestService.bored(mealId);
+            reloadData();
         };
     }
 })();
